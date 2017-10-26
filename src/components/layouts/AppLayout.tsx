bd60b7b9bc7fc2ba0b45a4539b8ff0096ba628ad
Trainings-Ncore <eugene0707@gmail.com>
2017-10-26 19:51:27 +0300

    Registration form

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,19 +1,29 @@
 import * as React from 'react';
-import Container from 'semantic-ui-react/dist/commonjs/elements/Container/Container';
 import Menu from 'semantic-ui-react/dist/commonjs/collections/Menu/Menu';
 import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon/Icon';
+import Header from 'semantic-ui-react/dist/commonjs/elements/Header/Header';
+import UserMenu from './UserMenu';
+import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid/Grid';
+import RegistrationForm from './RegistrationForm';
 
 export default class AppLayout extends React.Component {
     render() {
         return(
             <div>
-                <Menu pointing inverted style={{borderRadius: 0}}>
+                <Menu pointing inverted style={{borderRadius: 0}} color={'blue'}>
                     <Menu.Item>
-                        <Icon name={'settings'} size="large"/>
+                        <Icon name={'bars'} size="large"/>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Header as={'h2'} inverted content={'Bug Tracker'}/>
+                    </Menu.Item>
+                    <Menu.Item position={'right'}>
+                        <UserMenu/>
                     </Menu.Item>
                 </Menu>
-                <Container>
-                </Container>
+                <Grid container>
+                    <RegistrationForm/>
+                </Grid>
             </div>
         );
     }
